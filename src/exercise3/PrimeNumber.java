6b45bd4e59b1b91d604cf00f52409eac5f002a37
IvanDimov02 <>
2021-11-02 23:15:57 +0000

    Some message

--- src/exercise3/PrimeNumber.java
+++ src/exercise3/PrimeNumber.java
@@ -7,19 +7,19 @@ public class PrimeNumber {
   public static void main (String[] args) {
    Scanner input = new Scanner(System.in);
    
-   int n = input.nextInt();
+   int number = input.nextInt();
    boolean prime= true;
    int a = 2;
    
-   while (n > 2) {
-     if (n !=a && n % a == 0 ) {
+   while (number > 2) {
+     if (number !=a && number % a == 0 ) {
        prime = false;
        break;
      }
      if (a== 100) {
        break;
      }
-     a++;
+     a ++;
    }
    System.out.println("Prime number: " + prime);
   }
